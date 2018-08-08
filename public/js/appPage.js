6c6bc50ba79c3ab33bbe0b9d3451e49e7ccd6f9b
BrentVanant1999 <bvancar@yahoo.com>
2018-08-08 02:51:01 -0500

    Created setting user data functionality with Firebase as well as getting

--- public/js/appPage.js
+++ public/js/appPage.js
@@ -8,3 +8,156 @@ var durationInput = document.getElementById('durationInput');
 
 var searchButton = document.getElementById('searchEventButton');
 var listeningFirebaseRefs = [];
+
+/**
+ * Writes a new event
+ **/
+function writeEvent(uid, username, type, title, description) {
+  // A post entry.
+  var eventData = {
+    author: username,
+    uid: uid,
+    type: type,
+    title: title,
+    description: description
+  };
+
+  // Get a key for the new event.
+  var eventKey = firebase.database().ref().child('events').push().key;
+
+  // Write the new events's data simultaneously in the general events list and the users event list.
+  var updates = {};
+  updates['/events/' + newPostKey] = eventData;
+  updates['/user-events/' + uid + '/' + newPostKey] = eventData;
+
+  //return whether the database was updated.
+  return firebase.database().ref().update(updates);
+}
+
+/**
+ * creates thes actual event element.
+*/
+function createPostElement(postId, title, text, author, authorId, authorPic) {
+  var uid = firebase.auth().currentUser.uid;
+
+  var html =
+      '<div class="post post-' + postId + ' mdl-cell mdl-cell--12-col ' +
+                  'mdl-cell--6-col-tablet mdl-cell--4-col-desktop mdl-grid mdl-grid--no-spacing">' +
+        '<div class="mdl-card mdl-shadow--2dp">' +
+          '<div class="mdl-card__title mdl-color--light-blue-600 mdl-color-text--white">' +
+            '<h4 class="mdl-card__title-text"></h4>' +
+          '</div>' +
+          '<div class="header">' +
+            '<div>' +
+              '<div class="avatar"></div>' +
+              '<div class="username mdl-color-text--black"></div>' +
+            '</div>' +
+          '</div>' +
+          '<span class="star">' +
+            '<div class="not-starred material-icons">star_border</div>' +
+            '<div class="starred material-icons">star</div>' +
+            '<div class="star-count">0</div>' +
+          '</span>' +
+          '<div class="text"></div>' +
+          '<div class="comments-container"></div>' +
+          '<form class="add-comment" action="#">' +
+            '<div class="mdl-textfield mdl-js-textfield">' +
+              '<input class="mdl-textfield__input new-comment" type="text">' +
+              '<label class="mdl-textfield__label">Comment...</label>' +
+            '</div>' +
+          '</form>' +
+        '</div>' +
+      '</div>';
+
+  // Create the DOM element from the HTML.
+  var div = document.createElement('div');
+  div.innerHTML = html;
+  var postElement = div.firstChild;
+  if (componentHandler) {
+    componentHandler.upgradeElements(postElement.getElementsByClassName('mdl-textfield')[0]);
+  }
+
+  var addCommentForm = postElement.getElementsByClassName('add-comment')[0];
+  var commentInput = postElement.getElementsByClassName('new-comment')[0];
+  var star = postElement.getElementsByClassName('starred')[0];
+  var unStar = postElement.getElementsByClassName('not-starred')[0];
+
+  // Set values.
+  postElement.getElementsByClassName('text')[0].innerText = text;
+  postElement.getElementsByClassName('mdl-card__title-text')[0].innerText = title;
+  postElement.getElementsByClassName('username')[0].innerText = author || 'Anonymous';
+  postElement.getElementsByClassName('avatar')[0].style.backgroundImage = 'url("' +
+      (authorPic || './silhouette.jpg') + '")';
+
+  // Listen for comments.
+  // [START child_event_listener_recycler]
+  var commentsRef = firebase.database().ref('post-comments/' + postId);
+  commentsRef.on('child_added', function(data) {
+    addCommentElement(postElement, data.key, data.val().text, data.val().author);
+  });
+
+  commentsRef.on('child_changed', function(data) {
+    setCommentValues(postElement, data.key, data.val().text, data.val().author);
+  });
+
+  commentsRef.on('child_removed', function(data) {
+    deleteComment(postElement, data.key);
+  });
+  // [END child_event_listener_recycler]
+
+  // Listen for likes counts.
+  // [START post_value_event_listener]
+  var starCountRef = firebase.database().ref('posts/' + postId + '/starCount');
+  starCountRef.on('value', function(snapshot) {
+    updateStarCount(postElement, snapshot.val());
+  });
+  // [END post_value_event_listener]
+
+  // Listen for the starred status.
+  var starredStatusRef = firebase.database().ref('posts/' + postId + '/stars/' + uid);
+  starredStatusRef.on('value', function(snapshot) {
+    updateStarredByCurrentUser(postElement, snapshot.val());
+  });
+
+  // Keep track of all Firebase reference on which we are listening.
+  listeningFirebaseRefs.push(commentsRef);
+  listeningFirebaseRefs.push(starCountRef);
+  listeningFirebaseRefs.push(starredStatusRef);
+
+  // Create new comment.
+  addCommentForm.onsubmit = function(e) {
+    e.preventDefault();
+    createNewComment(postId, firebase.auth().currentUser.displayName, uid, commentInput.value);
+    commentInput.value = '';
+    commentInput.parentElement.MaterialTextfield.boundUpdateClassesHandler();
+  };
+
+  // Bind starring action.
+  var onStarClicked = function() {
+    var globalPostRef = firebase.database().ref('/posts/' + postId);
+    var userPostRef = firebase.database().ref('/user-posts/' + authorId + '/' + postId);
+    toggleStar(globalPostRef, uid);
+    toggleStar(userPostRef, uid);
+  };
+  unStar.onclick = onStarClicked;
+  star.onclick = onStarClicked;
+
+  return postElement;
+}
+
+
+/**
+ * Creates a new post for the current user.
+ */
+function newEventCurrentUserCreating(title, text) {
+  // [START single_value_read]
+  var userId = firebase.auth().currentUser.uid;
+  return firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
+    var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
+    // [START_EXCLUDE]
+    return writeNewPost(firebase.auth().currentUser.uid, username,
+      lat, lng, title, text);
+    // [END_EXCLUDE]
+  });
+  // [END single_value_read]
+}
--- public/js/firebase/handleUser.js
+++ public/js/firebase/handleUser.js
@@ -1,19 +1,52 @@
+
 initApp = function() {
   firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
       // User is signed in.
-      var displayName = user.displayName;
-      var email = user.email;
-      var uid = user.uid;
-      console.log(uid);
-      user.getIdToken().then(function(accessToken) {
-        document.getElementById('app-header').textContent = 'Signed in';
-      });
+       var displayName = user.displayName;
+       var email = user.email;
+       var uid = user.uid;
+         firebase.database();
+       writeUserData(uid, email, displayName);
+       displayUserData(uid, displayName);
+       startDatabaseQueries();
     }
   }, function(error) {
     console.log(error);
   });
 };
+
+function startDatabaseQueries() {
+  var myUserId = firebase.auth().currentUser.uid;
+}
+
+function writeUserData(userId, email, displayName ) {
+
+  firebase.database().ref('users/' + userId).set({
+    username: displayName,
+    email: email
+  });
+}
+function displayUserData(userId, displayName ) {
+  const idPlacement = document.getElementById('user-id-placement');
+  const usernamePlacement = document.getElementById('user-name-placement');
+//  const getInfoButton = document.getElementById('get-info');
+//  const eventInfoPlacement = document.getElementById('event-info-placement');
+  idPlacement.textContent = userId;
+  usernamePlacement.textContent = displayName;
+}
+function writeEventData(ownerId, ownerName, title, type, description, time, duration, location) {
+  firebase.database().ref('events/' + userId).set({
+    ownerName: displayName,
+    eventTitle: title,
+    eventType: type,
+    eventDescription: description,
+    eventTime: time,
+    eventDuration: duration,
+    eventLocation: location
+  });
+}
+
 window.addEventListener('load', function() {
   initApp()
 });
