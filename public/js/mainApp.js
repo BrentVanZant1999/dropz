b0e103a952e3dca782a62276291ab65c34493f46
BrentVanant1999 <bvancar@yahoo.com>
2018-08-10 21:17:58 -0500

    Event display fully complete

--- public/js/mainApp.js
+++ public/js/mainApp.js
@@ -3,22 +3,34 @@ const locateZoom = 18;
 const maxMapZoom = 20;
 const mymap = L.map('mainMap').setView([32.88025,-117.23752],locateZoom);
 var inModal = false;
-var markerGroupUI = L.layerGroup().addTo(mymap);
+var hasLocation = false;
+var tempLat = 0;
+var tempLng = 0;
+
 var markerGroupEvents = L.layerGroup().addTo(mymap);
+var markerGroupUI;
+var markerUI;
 
 eventMarker = L.Marker.extend({
-options: {
-title: '',
-org: '',
-type: '',
-description:'',
-time:''
-}
+  options: {
+      title: '',
+      org: '',
+      type: '',
+      description:'',
+      time:''
+    }
 });
 
 mymap.on('locationerror', onLocationError);
 mymap.on('locationfound', onLocationfound);
 
+//setup the ui icon definitions
+var creatingIcon = L.icon({
+    iconUrl: 'css/markerCreating.png',
+    iconSize:     [64, 64], // size of the icon
+    iconAnchor:   [32, 64], // point of the icon which will correspond to marker's location
+    popupAnchor:  [32, 10] // point from which the popup should open relative to the iconAnchor
+});
 var academicIcon = L.icon({
     iconUrl: 'css/markerAcademic.png',
     iconSize:     [64, 64], // size of the icon
@@ -44,16 +56,6 @@ var sportingIcon = L.icon({
     popupAnchor:  [32, 10] // point from which the popup should open relative to the iconAnchor
 });
 
-//initiate a user marker with a temporary location
-var userCreateEventMarker = L.marker([0,0], {
-    draggable:true,
-    //todo make create event icon
-}).addTo(markerGroupUI);
-//handle drag events
-userCreateEventMarker.on('dragend', function(event){
-    var userCreateEventMarker = event.target;
-});
-
 //put a tile layer on the map
 L.tileLayer('https://api.mapbox.com/styles/v1/bvanzant/cjitaouuo4toq2so62d2nn724/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYnZhbnphbnQiLCJhIjoiY2ppZTZhdzh2MDZvazN3bXllOTlmYXc4aCJ9.ipjbP-7psE4EN1sVYotlsQ', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -66,27 +68,121 @@ L.tileLayer('https://api.mapbox.com/styles/v1/bvanzant/cjitaouuo4toq2so62d2nn724
 $('#locator').on('click', function(){
   locateClientUser();
 });
+
 $('#eventCreateButton').on('click', function(){
   if (inModal === false) {
-    $('#eventModal').modal('show');
+    if (hasLocation === false) {
+      createNewEventMarker();
+      toggleButtons();
+    }
+    else {
+      $('#eventCreateModal').modal('show');
+      var latLng = markerUI.getLatLng();
+      tempLng = latLng.lng;
+      tempLat = latLng.lat;
+      console.log(tempLng);
+      console.log(tempLat);
+      inModal = true;
+      toggleButtons();
+    }
   }
 });
 
+$('#eventCancelButton').on('click', function(){
+  cancelButton();
+});
+
+//cancelButton
+function cancelButton(){
+  toggleButtons();
+  mymap.removeLayer(markerGroupUI);
+  inModal = false;
+  hasLocation = false;
+}
+/*
+ * Create a new event markers
+ */
+function createNewEventMarker() {
+  //initiate a user marker with a temporary location
+  markerGroupUI = L.layerGroup().addTo(mymap);
+  markerUI = L.marker(mymap.getCenter(), {
+      draggable:true,
+      icon:creatingIcon
+  }).addTo(markerGroupUI);
+  //handle drag events
+  markerUI.on('dragend', function(event){
+      var userCreateEventMarker = event.target;
+  });
+}
+/*
+ * Update the event modal
+ */
+function updateEventModal(title,organization,description,type) {
+  //initiate a user marker with a temporary location
+  document.getElementById("eventTitlePlace").innerHTML = title;
+  document.getElementById("eventOrganizationPlace").innerHTML = organization;
+  document.getElementById("eventInfoDescription").innerHTML = description;
+  document.getElementById("eventTypePlace").classList.remove('fa-book');
+  document.getElementById("eventTypePlace").classList.remove('fa-basketball-ball');
+  document.getElementById("eventTypePlace").classList.remove('fa-tag');
+  document.getElementById("eventTypePlace").classList.remove('fa-users');
+  switch(type){
+    case(1):
+    {
+      document.getElementById("eventTypePlace").classList.add('fa-users');
+    }
+    break;
+    case(2):
+    {
+      document.getElementById("eventTypePlace").classList.add('fa-basketball-ball');
+    }
+    break;
+    case(3):
+    {
+      document.getElementById("eventTypePlace").classList.add('fa-book');
+    }
+    break;
+    case(4):
+    {
+      document.getElementById("eventTypePlace").classList.add('fa-tag');
+    }
+    break;
+  }
+}
+
+/*
+ * function to toggle buttons to remove and add styles to buttons.
+ */
+function toggleButtons(){
+    if (hasLocation === false){
+      document.getElementById("eventCreateButtonIcon").classList.remove('fa-plus');
+      document.getElementById("eventCreateButtonIcon").classList.add('fa-check');
+      document.getElementById("eventCancelButton").classList.remove('invis');
+      document.getElementById("eventCancelButton").classList.add('vis');
+      hasLocation = true;
+    }
+    else {
+      document.getElementById("eventCreateButtonIcon").classList.remove('fa-check');
+      document.getElementById("eventCreateButtonIcon").classList.add('fa-plus');
+      document.getElementById("eventCancelButton").classList.remove('vis');
+      document.getElementById("eventCancelButton").classList.add('invis');
+      hasLocation = false;
+    }
+}
 //map functions
 function locateClientUser() {
   mymap.locate({setView:true, mapZoom:locateZoom});
 }
 function onLocationfound(e){
-    userCreateEventMarker.setLatLng(e.latlng);
+  userCreateEventMarker.setLatLng(e.latlng);
 }
 function onLocationError(e) {
-    alert("Location Database error");
-  }
+  alert("Location Database error");
+}
 
 initApp = function() {
   firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
-      // User is signed in.
        var displayName = user.displayName;
        var email = user.email;
        var uid = user.uid;
@@ -118,89 +214,71 @@ function startDatabaseQuery() {
 }
 
 /*
- * Create a marker class.
-
-eventMarker = L.Marker.extend({
-   options: {
-      title: '',
-      org:'',
-      endTime: '',
-      type: 0,
-      description: ''
-   }
-});
-*/
-/*
  * Create a marker that store value about an event.
  */
 function createEventMarker(lat, lng, titleIn, orgIn, typeIn, timeIn, descriptIn){
-switch(typeIn){
-  case(1):
-  {
-    var newMarker = new eventMarker([lat, lng],{
-      clickable: true,
-      title: titleIn,
-      org: orgIn,
-      type: typeIn,
-      description: descriptIn,
-      time: timeIn,
-      icon: socialIcon
-    }).addTo(markerGroupEvents);
+  var newMarker;
+  switch(typeIn){
+    case(1):
+    {
+      newMarker = new eventMarker([lat, lng],{
+        clickable: true,
+        title: titleIn,
+        org: orgIn,
+        type: typeIn,
+        description: descriptIn,
+        time: timeIn,
+        icon: socialIcon
+      }).on('click', handleMarkerClick);
+    }
+    break;
+    case(2):
+    {
+      newMarker = new eventMarker([lat, lng],{
+        clickable: true,
+        title: titleIn,
+        org: orgIn,
+        type: typeIn,
+        description: descriptIn,
+        time: timeIn,
+        icon: sportingIcon
+      }).on('click', handleMarkerClick);
+    }
+    break;
+    case(3):
+    {
+      newMarker = new eventMarker([lat, lng],{
+        clickable: true,
+        title: titleIn,
+        org: orgIn,
+        type: typeIn,
+        description: descriptIn,
+        time: timeIn,
+        icon: academicIcon
+      }).on('click', handleMarkerClick);
+    }
+    break;
+    case(4):
+    {
+      newMarker = new eventMarker([lat, lng],{
+        clickable: true,
+        title: titleIn,
+        org: orgIn,
+        type: typeIn,
+        description: descriptIn,
+        time: timeIn,
+        icon: marketIcon
+      }).on('click', handleMarkerClick);
+    }
+    break;
   }
-  break;
-  case(2):
-  {
-    var newMarker = new eventMarker([lat, lng],{
-      clickable: true,
-      title: titleIn,
-      org: orgIn,
-      type: typeIn,
-      description: descriptIn,
-      time: timeIn,
-      icon: sportingIcon
-    }).addTo(markerGroupEvents);
-  }
-  break;
-  case(3):
-  {
-    var newMarker = new eventMarker([lat, lng],{
-      clickable: true,
-      title: titleIn,
-      org: orgIn,
-      type: typeIn,
-      description: descriptIn,
-      time: timeIn,
-      icon: academicIcon
-    }).addTo(markerGroupEvents);
-  }
-  break;
-  case(4):
-  {
-    var newMarker = new eventMarker([lat, lng],{
-      clickable: true,
-      title: titleIn,
-      org: orgIn,
-      type: typeIn,
-      description: descriptIn,
-      time: timeIn,
-      icon: marketIcon
-    }).addTo(markerGroupEvents);
-  }
-  break;
+  newMarker.addTo(markerGroupEvents);
 }
 
- /*
-  var newMarker = new eventMarker([lat,lng]),{
-    title:titleIn,
-    org:orgIn,
-    type:typeIn,
-    endTime:timeIn,
-    description:descriptIn
-  }).addTo(markerGroupEvents);
-  */
+function handleMarkerClick(e) {
+  updateEventModal(this.options.title,this.options.org,this.options.description,this.options.type);
+  $('#modalEventInfo').modal('show');
 }
-
-
 
 function validateEventInput() {
   var titleInput = document.getElementById("titleEventInput");
@@ -237,15 +315,6 @@ function writeUserData(userId, email, displayName ) {
   });
 }
 
-/*
-function displayUserData(userId, displayName ) {
-  const idPlacement = document.getElementById('user-id-placement');
-  const usernamePlacement = document.getElementById('user-name-placement');
-  idPlacement.textContent = userId;
-  usernamePlacement.textContent = displayName;
-}
-*/
-
 function writeEventData(ownerId, ownerName, title, type, description, time, duration, location) {
   firebase.database().ref('events/' + userId).set({
     ownerName: displayName,
@@ -257,8 +326,6 @@ function writeEventData(ownerId, ownerName, title, type, description, time, dura
     eventLocation: location
   });
 }
-
-
 
 window.addEventListener('load', function() {
   initApp();
