6d26521681504829a3a8bca3d9f75662d16300e0
BrentVanant1999 <bvancar@yahoo.com>
2018-08-10 22:35:04 -0500

    Changed port to environmental port

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express   =     require("express");
 var app =  express();
+const PORT = process.env.PORT || 8000;
 app.use(express.static(__dirname + '/public'));
 app.get('/',function(req, res) {
     res.sendFile(__dirname + '/public/login.html');
@@ -7,5 +8,5 @@ app.get('/',function(req, res) {
 app.get('/app', function (req, res) {
  res.sendfile(__dirname + '/public/app.html');
 });
-app.listen(3001);
+app.listen(PORT);
 console.log("Server started.");
